ed27ee7b0900a4630c64ccf01330027354706534
Qin Yue Chen <hugulas@163.com>
2019-07-22 07:05:57 -0400

    mem.c with vps support

--- linux-perf/sample2/mem.c
+++ linux-perf/sample2/mem.c
@@ -1,15 +1,27 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+	int n = atoi(argv[1]);
+	int enough = 1;
         while(1)
         {
+		
 		// use 1MB memory
-                void *m = malloc(1024*1024);
-                memset(m,0,1024*1024);
-		// sleep 1 second
-		sleep(1000);
+        	if (enough) {        
+			void *m = malloc(1024*1024);
+                	if (!m) {
+	           		enough = 0;
+		   		printf("No more memory to use");
+			}
+			else {
+		   		memset(m,0,1024*1024);
+			}
+		}
+		// sleep n micro seconds
+		usleep(n);
         }
         return 0;
 }
